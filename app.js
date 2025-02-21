b79bca9b3eebb7c1077d9c9d474f8f32ed8621fb
ManoharNaidu <beesettim27@gmail.com>
2025-02-21 08:49:35 +0530

    id -> _id

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
-app.use("/api/v1/workflow", workflowRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.get("/", (req, res) => {
   res.send(`Welcome to the Subscription Tracker API`);
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -12,9 +12,9 @@ export const createSubscription = async (req, res, next) => {
     // console.log(`${SERVER_URL}/api/v2/workflow/subscription/reminder`);
 
     const { workflowRunId } = await workflowClient.trigger({
-      url: `${SERVER_URL}/api/v1/workflow/subscription/reminder`,
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
       body: {
-        subscriptionId: subscription.id,
+        subscriptionId: subscription._id,
       },
       headers: {
         "content-type": "application/json",
@@ -25,26 +25,24 @@ export const createSubscription = async (req, res, next) => {
     res
       .status(201)
       .json({ success: true, data: { subscription, workflowRunId } });
-  } catch (error) {
-    next(error);
+  } catch (e) {
+    next(e);
   }
 };
 
 export const getUserSubscriptions = async (req, res, next) => {
   try {
-    if (req.user._id != req.params.id) {
-      const error = new Error("You're unauthorized to access");
+    // Check if the user is the same as the one in the token
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
       error.status = 401;
       throw error;
     }
 
     const subscriptions = await Subscription.find({ user: req.params.id });
 
-    res.status(200).json({
-      success: true,
-      data: subscriptions,
-    });
-  } catch (error) {
-    next(error);
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
   }
 };
